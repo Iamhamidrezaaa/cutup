31ba546fa8288a15ab1d769e8dd2b132abc154f4
Iamhamidrezaaa <h.asgarizade@gmail.com>
2026-05-29 17:18:29 +0330

    Update ffmpeg command and subtitle filter for ASS compatibility
    This commit modifies the `ffmpeg-cutup-command.sh` and `forensic-report.json` files to replace the 'subtitles' filter with 'ass' in the ffmpeg command and subtitle filter string. This change enhances compatibility with the ASS subtitle format, ensuring a more accurate rendering of subtitles in the output video.

--- forensic-rtl-sample/ffmpeg-cutup-command.sh
+++ forensic-rtl-sample/ffmpeg-cutup-command.sh
@@ -1,1 +1,1 @@
-ffmpeg -hide_banner -y -i INPUT.mp4 -vf "scale=1080:1920,subtitles=subtitles.ass:original_size=1080x1920" -c:v libx264 -preset fast -crf 23 -c:a aac OUT_cutup_ass.mp4
+ffmpeg -hide_banner -y -i INPUT.mp4 -vf "scale=1080:1920,ass=subtitles.ass" -c:v libx264 -preset fast -crf 23 -c:a aac OUT_cutup_ass.mp4
